730b7e8dd8a8512f8625965769717d0c87b44086
Georg G <nilsding@nilsding.org>
2015-10-26 20:23:58 +0100

    Initial commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Modemspring_next
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Modemspring_next.MvcApplication" Language="C#" %>
